9128b96f59cc169ef6689df56241fe9c3b374f59
Tharinee Munkwamdee <tharineemunk@Tharinees-MacBook-Air.local>
2018-06-04 16:16:39 +0700

    case number, add tested!

--- Calculator.js
+++ Calculator.js
@@ -2,9 +2,14 @@ class Calculator {
   constructor() {
     this.str = '';
   }
+  
   calculate(str) {
     this.str = str;
-    return this.str.length;
+    if (str === '') return 0;
+    else if (typeof str === 'number') return str;
+    else if (str.indexOf(',') !== -1) return Number(str.split(',')[0]) + Number(str.split(',')[1]);
+    else if (str.indexOf('\n') !== -1) return Number(str.split('\n')[0]) + Number(str.split('\n')[1]);
+    return -1;
   }
 }
 
--- test/test-string-calculator.js
+++ test/test-string-calculator.js
@@ -2,6 +2,7 @@ const Calculator = require('../Calculator');
 const chai = require('chai');
 
 const { expect } = chai;
+const cal1 = new Calculator();
 
 // Start test cases for Example Behaviors
 function itAlwaysTrue() {
@@ -13,21 +14,29 @@ function itAlwaysBe2() {
   expect(2).to.be.equal(2);
 }
 
+function itShouldBeZero() {
+  expect(cal1.calculate('')).to.be.equal(0);
+}
+
+function itShouldBeNumber() {
+  const x = Math.floor(Math.random() * 10000);
+  expect(typeof cal1.calculate(x)).to.be.equal('number');
+  expect(cal1.calculate(x)).to.be.equal(x);
+}
+
+function itShouldBeSum() {
+  expect(cal1.calculate('1,2')).to.be.equal(3);
+  expect(cal1.calculate('1\n2')).to.be.equal(3);
+}
+
 // Start Example Behaviors
 function exampleBehaviors() {
   it('should be true', itAlwaysTrue);
   it('should be equal 2', itAlwaysBe2);
-}
-
-function itShouldBeZero() {
-  const cal1 = new Calculator();
-  expect(cal1.calculate('')).to.be.equal(0);
-}
-
-function testEmptyString() {
   it('should be zero', itShouldBeZero);
+  it('should be number', itShouldBeNumber);
+  it('should be sum', itShouldBeSum);
 }
 
 // Start Describe
 describe('Example', exampleBehaviors);
-describe('Empty string should return zero', testEmptyString);
